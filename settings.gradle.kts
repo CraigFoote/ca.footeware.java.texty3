b442778a2fd6724b5cefd7e02a749e79db66c37d
CraigFoote <CraigFoote@gmail.com>
2025-04-03 22:29:45 -0400

    flatpak build

--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "texty3"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven { url=uri("build/repository") } // used by flatpak-builder
-    }
-}
